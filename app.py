683e354e6f2ccd81c207edea44fe15aa07104f16
Emetex180 <emediongedoho1@gmail.com>
2025-08-26 16:02:16 +0100

    Prepare app for Render deployment

--- app.py
+++ app.py
@@ -237,8 +237,6 @@ def next():
     return render_template('next.html')
 
 # ----------------- Run -----------------
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
+# Only run this block when running locally
+# if __name__ == '__main__':
+#     app.run(debug=True)
